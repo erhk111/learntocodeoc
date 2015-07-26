3d6100e1683618c332441539b0463b965b9c1d85
ronperris <ronperris@gmail.com>
2015-07-25 23:29:15 -0700

    Added some tests.

---
+++ test.js
@@ -0,0 +1,14 @@
+var chai = require('chai');
+
+var expect = chai.expect;
+var should = chai.should();
+
+describe("Run some tests to", function() {
+  it('1 should equal 1', function() {
+    expect(1).to.be.equal(1);
+  });
+
+  it('2 should equal 3', function() {
+    expect(2).to.be.equal(3);
+  });
+});
